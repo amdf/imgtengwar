05fcaea1cd9cee8dad6388ae9e450cb118bd1f61
amdf <787535+amdf@users.noreply.github.com>
2021-08-16 20:18:45 +0300

    pass text via param

--- main.go
+++ main.go
@@ -8,9 +8,8 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	ti := TengwarImage{}
-	ti.Init()
-	mux.HandleFunc("/txt/", ti.ConvertText)
+	ti := NewTengwarImage()
+	mux.HandleFunc("/text/", ti.ConvertText)
 	port := os.Getenv("IMGTENGWAR_PORT")
 	log.Print("Listen on port ", port)
 	log.Fatal(http.ListenAndServe("localhost:"+port, mux))
--- mkimg.go
+++ mkimg.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -9,15 +10,37 @@ import (
 
 //TengwarImage handles img creation
 type TengwarImage struct {
-	conv rustengwar.Converter
+	Conv rustengwar.Converter
 }
 
-func (ti *TengwarImage) Init() {
-	ti.conv.InitDefault()
+//NewTengwarImage creates TengwarImage
+func NewTengwarImage() (ti *TengwarImage) {
+	ti = new(TengwarImage)
+	ti.Conv.InitDefault()
+	return
 }
+
+func (ti TengwarImage) getSingleParam(req *http.Request, key string) string {
+	keys, ok := req.URL.Query()[key]
+
+	if !ok || len(keys[0]) < 1 {
+		return ""
+	}
+	return keys[0]
+}
+
+//ConvertText shows converted text
 func (ti *TengwarImage) ConvertText(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(w, fmt.Sprintf("expect method GET /text/, got %v", req.Method), http.StatusMethodNotAllowed)
+		return
+	}
+
 	log.Printf("handling ConvertText at %s\n", req.URL.Path)
+
+	text := ti.getSingleParam(req, "text")
+
 	//TODO: w.Header().Set("Content-Type", "")
-	s, _ := ti.conv.Convert("прекрасно")
+	s, _ := ti.Conv.Convert(text)
 	w.Write([]byte(s))
 }
