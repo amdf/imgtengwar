f5e424a84ed16eeb696b9b28951ce76cf2e3a597
amdf <787535+amdf@users.noreply.github.com>
2022-02-14 16:36:58 +0300

    implement as gRPC

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/amdf/imgtengwar
 go 1.16
 
 require (
-	github.com/amdf/rustengwar v0.1.2
-	github.com/gin-gonic/gin v1.7.4
 	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.3
 	golang.org/x/image v0.0.0-20210628002857-a66eb6448b8d
+	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8
+	google.golang.org/genproto v0.0.0-20220211171837-173942840c17
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.27.1
 )
--- main.go
+++ main.go
@@ -1,26 +1,120 @@
 package main
 
 import (
+	"context"
 	"log"
-	"os"
+	"net"
+	"net/http"
 
 	"github.com/amdf/imgtengwar/internal/render"
-	"github.com/gin-gonic/gin"
+	pb "github.com/amdf/imgtengwar/svc"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"golang.org/x/time/rate"
+	"google.golang.org/genproto/googleapis/api/httpbody"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/tap"
 )
 
+const (
+	SVCRPCADDR  = "localhost:50051"
+	SVCHTTPADDR = "localhost:8081" //"0.0.0.0:80"
+)
+
+type TengwarConverterServer struct {
+	pb.UnimplementedTengwarConverterServer
+	clientLimits map[string]*rate.Limiter
+}
+
+func (srv TengwarConverterServer) ConvertText(context.Context, *pb.SimpleConvertRequest) (*pb.ConvertResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ConvertText not implemented")
+}
+func (srv TengwarConverterServer) MakeImage(context.Context, *pb.ConvertRequest) (*httpbody.HttpBody, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method MakeImage not implemented")
+}
+
+func (srv TengwarConverterServer) GetClientAddr(ctx context.Context) string {
+	ipaddr := "(unknown)"
+	p, ok := peer.FromContext(ctx)
+	if ok {
+		ipaddr = p.Addr.String()
+	}
+	return ipaddr
+}
+
+func (srv TengwarConverterServer) RateLimiter(ctx context.Context, info *tap.Info) (context.Context, error) {
+	user := srv.GetClientAddr(ctx)
+	if srv.clientLimits[user] == nil {
+		//TODO: store limits in config
+		srv.clientLimits[user] = rate.NewLimiter(10, 50) // QPS, burst
+	}
+
+	if !srv.clientLimits[user].Allow() {
+		return nil, status.Errorf(codes.ResourceExhausted,
+			"client exceeded rate limit")
+	}
+	return ctx, nil
+}
+
+func runGateway() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Note: Make sure the gRPC server is running properly and accessible
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+
+	err := pb.RegisterTengwarConverterHandlerFromEndpoint(ctx, mux, SVCRPCADDR, opts)
+	if err != nil {
+		log.Fatalln("fail to register http", err)
+	}
+
+	log.Println("starting http server at ", SVCHTTPADDR)
+
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	if err != http.ListenAndServe(SVCHTTPADDR, mux) {
+		log.Fatalln("fail to serve http", err)
+	}
+}
+
+func MakeTengwarConverterServer() (srv *TengwarConverterServer, err error) {
+	srv = &TengwarConverterServer{}
+	srv.clientLimits = make(map[string]*rate.Limiter)
+	return
+}
+
 func main() {
-	render.Init()
+	err := render.Init()
+	if err != nil {
+		log.Fatalln("fail to create render", err)
+	}
 
-	server := TengwarImageServer{}
-	server.Init()
+	server, err := MakeTengwarConverterServer()
+	if err != nil {
+		log.Fatalln("fail to create server", err)
+	}
 
-	router := gin.Default()
+	lis, err := net.Listen("tcp", SVCRPCADDR)
+	if err != nil {
+		log.Fatalln("fail to listen", err)
+	}
 
-	router.GET("/text", server.ConvertText)
-	router.GET("/img", server.ConvertImage)
+	s := grpc.NewServer(
+		grpc.InTapHandle(server.RateLimiter),
+	)
+	pb.RegisterTengwarConverterServer(s, server)
 
-	port := os.Getenv("IMGTENGWAR_PORT")
-	log.Print("Listen on port ", port)
+	log.Println("starting server at ", lis.Addr())
 
-	log.Fatal(router.Run("localhost:" + port))
+	go runGateway()
+
+	err = s.Serve(lis)
+	if err != nil {
+		log.Fatalln("fail to server", err)
+	}
+
 }
