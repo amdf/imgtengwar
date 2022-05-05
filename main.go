dc5be14d2d37087ccc7470386e158b12dfbc9a73
amdf <787535+amdf@users.noreply.github.com>
2022-05-05 21:35:33 +0300

    cors for swagger

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -95,25 +96,65 @@ func (srv TengwarConverterServer) RateLimiter(ctx context.Context, info *tap.Inf
 }
 
 func (srv TengwarConverterServer) RunGateway() {
+	//TODO: handle errors another way
+
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
 	// Note: Make sure the gRPC server is running properly and accessible
 	mux := runtime.NewServeMux()
-	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 
-	err := pb.RegisterTengwarConverterHandlerFromEndpoint(ctx, mux, SVCRPCADDR, opts)
+	conn, err := grpc.DialContext(
+		ctx,
+		SVCRPCADDR,
+		//grpc.WithInsecure(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("fail to dial rpc", err)
+	}
+	err = pb.RegisterTengwarConverterHandler(ctx, mux, conn)
 	if err != nil {
 		log.Fatalln("fail to register http", err)
 	}
+	//TODO: move this to config
+	allowedOrigins := []string{
+		"http://localhost:8080",
+		"http://127.0.0.1:8080"}
 
 	log.Println("starting http server at ", SVCHTTPADDR)
-
+	gatewayServer := &http.Server{
+		Addr:    SVCHTTPADDR,
+		Handler: cors(mux, allowedOrigins),
+	}
 	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	if err != http.ListenAndServe(SVCHTTPADDR, mux) {
+	if err = gatewayServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
 		log.Fatalln("fail to serve http", err)
 	}
+}
+
+func cors(h http.Handler, allowedOrigins []string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		providedOrigin := r.Header.Get("Origin")
+		matches := false
+		for _, allowedOrigin := range allowedOrigins {
+			if providedOrigin == allowedOrigin {
+				matches = true
+				break
+			}
+		}
+
+		if matches {
+			w.Header().Set("Access-Control-Allow-Origin", providedOrigin)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType, grpc-metadata-log-level")
+		}
+		if r.Method == "OPTIONS" {
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
 
 func MakeTengwarConverterServer() (srv *TengwarConverterServer, err error) {
